6b20078e0933cf6339f68e7f95def83cb3ccc8ee
PaperLily12 <87453778+PaperLily12@users.noreply.github.com>
2022-08-03 20:19:51 -0500

    Delete GasMileage.cs

--- Part2/GasMileage.cs
+++
@@ -1,3 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("What is the Miles per Gallon of your car?");
-int MilesPerGallon = int.Parse(Console.ReadLine());
