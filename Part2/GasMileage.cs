2b58000a918db9e481abbd93e950f997503e47a8
Brigid <brigidbook@gmail.com>
2022-08-03 20:16:13 -0500

    Add project files.

---
+++ Part2/GasMileage.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is the Miles per Gallon of your car?");
+int MilesPerGallon = int.Parse(Console.ReadLine());
---
+++ Studio1/AreaOfCircle.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What should the radius of the circle be?");
+double radius = float.Parse(Console.ReadLine());
+double area = Math.PI * Math.Pow(radius, 2);
+double circumference = Math.PI * radius * 2;
+
+Console.WriteLine($"The area of the circle is {area}");
+Console.WriteLine($"The circumference of the circle is {circumference}");
+Console.WriteLine($"The diameter of the circle is {radius*2}");
+
+Console.WriteLine("What is the Miles per Gallon of your car?");
+double MilesPerGallon = double.Parse(Console.ReadLine());
+double gallonsNeeded = circumference / MilesPerGallon;
+Console.WriteLine($"You would need {gallonsNeeded} gallons of gas to travel around the circle.");
